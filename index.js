4d291fce0c94c8c1d4cafe7babe8907c8b46d405
bikmay <artyom.ivliev@gmail.com>
2020-08-12 12:42:46 +0400

    fix confirm

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const VkBot = require('node-vk-bot-api/lib');
 const app = express();
 const bot = new VkBot({
   token: '298e70a4126c2bea12cdd68a27876f0d7749c69c651d11a3981b0fec11878c0cdf3142e0842b6d210a05a',
-  confirmation: 'e4766b31',
+  confirmation: 'b4b22563',
 });
 
 bot.on((ctx) => {
